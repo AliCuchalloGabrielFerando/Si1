1e873646e30d82260892d8cf86a15ef06757e6c9
AliCuchalloGabrielFerando <f1.gabriel73@gmail.com>
2020-10-16 15:25:11 -0400

    cuarta

--- app/Http/Controllers/DetalleComprasController.php
+++ app/Http/Controllers/DetalleComprasController.php
@@ -45,7 +45,8 @@ class DetalleComprasController extends Controller
         $detalleCompra->precio = $detalleCompra->cantidad * $detalleCompra->precio_unitario;
         $detalleCompra->compra_id = $compra_id;
 
-        $libro = Libro::where('titulo',$request->input('libro_nombre'))->get();
+        $libro_titulo= explode(' Edición',$request->input('libro_nombre'));
+        $libro = Libro::where('titulo',$libro_titulo)->get();
         if(sizeof($libro) == 0 ){
         return redirect()->route('libros.create');
         }
