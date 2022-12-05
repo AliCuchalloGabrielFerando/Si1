394ac3a752641010d5c93f0f14a04098486297df
AliCuchalloGabrielFernando <71860613+AliCuchalloGabrielFerando@users.noreply.github.com>
2022-12-05 11:19:20 -0400

    preparando para fly.io

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(EstadoSeeder::class);
+       // $this->call(EstadoSeeder::class);
         //$this->call(DescuentoSeeder::class);
     }
 }
